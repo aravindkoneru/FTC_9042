fcd9debd71d2017f57c8acd4d40f41e5477eb580
TimMagoun <timmagoun@gmail.com>
2015-10-31 10:57:49 -0400

    Minor changes

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DriveStickWithHelper.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by Tim on 10/25/2015.
+ */
+public class DriveStickWithHelper extends OpHelper{
+    public DriveStickWithHelper()
+    {}
+
+    public void loop()
+    {
+    }
+}
